335b063467ce5ef8812f41f2aedb609f57c9dc20
Xavi Jorda <x.jorda@cognigy.com>
2023-05-13 12:25:19 +0200

    1st commit

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,86 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry := createRandomEntry(t, account)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry.ID, entry2.ID)
+	require.Equal(t, entry.AccountID, entry2.AccountID)
+	require.Equal(t, entry.Amount, entry2.Amount)
+	require.WithinDuration(t, entry.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry := createRandomEntry(t, account)
+
+	err := testQueries.DeleteEntry(context.Background(), entry.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetEntry(context.Background(), entry.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i<10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams {
+		AccountID: account.ID,
+		Limit: 5,
+		Offset: 5,
+	}
+	
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry:= range entries {
+		require.NotEmpty(t, entry)
+	}
+}
