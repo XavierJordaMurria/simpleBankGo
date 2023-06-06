88e04399d100bf2377544fb55619476478b96f14
Xavi Jorda <x.jorda@cognigy.com>
2023-06-06 08:12:56 +0200

    adding transfer API

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/xjorda/simplebank/db/sqlc"
 )
 
@@ -16,9 +18,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
@@ -31,4 +39,3 @@ func (server *Server) Start(address string) error {
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
-
