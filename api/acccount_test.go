7b228e77deeb5dd9050b416d55bdc158759ccff9
Xavi Jorda <x.jorda@cognigy.com>
2023-06-25 18:03:31 +0200

    fixing tests

--- api/acccount_test.go
+++ api/acccount_test.go
@@ -93,7 +93,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStub(store)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%v", tc.accountID)
